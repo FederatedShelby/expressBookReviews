bdfdd333a4016faf527e82ba37f0c9bd0013d354
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-10-31 14:47:02 -0700

    feat(reviews): get reviews for book matching given ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -89,9 +89,26 @@ public_users.get("/title/:title", function (req, res) {
 });
 
 //  Get book review
+// NOTE: each user can only add/modify 1 review per book
+// NOTE: each book can have N reviews by N users
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  if (!!books) {
+    const isbn = req.params.isbn;
+    const matchedBook = books[isbn];
+
+    if (!!matchedBook) {
+      const reviews = matchedBook.reviews;
+      
+      if (Object.keys(reviews).length > 0) {
+        return res.status(200).json(reviews);
+      }
+      return res.status(200).json({ message: "No reviews found for this book" });
+    }
+    return res.status(200).json({ message: "No book found for given ISBN" });
+  }
+  return res
+    .status(404)
+    .json({ message: "Error: unable to access books list" });
 });
 
 module.exports.general = public_users;
