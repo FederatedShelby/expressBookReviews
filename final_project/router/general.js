15f81e8569e0dd1b7aec6dc2d98052d1cfd851cc
AAL-MLOps-Shelby-S <nullterra180@gmail.com>
2024-10-31 14:21:11 -0700

    feat(title): get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,8 +73,19 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  if (!!books) {
+    const title = req.params.title;
+    const booksList = Object.values(books);
+    const matchedBooks = booksList.filter((book) => book.title === title);
+
+    if (matchedBooks.length > 0) {
+      return res.status(200).json({ matchedBooks });
+    }
+    return res.status(200).json({ message: "No book found for given title" });
+  }
+  return res
+    .status(404)
+    .json({ message: "Error: unable to access books list" });
 });
 
 //  Get book review
